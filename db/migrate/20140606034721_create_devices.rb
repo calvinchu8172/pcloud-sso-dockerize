2cf355288ce91dcc1da6bafa20f2b90138062a12
travisliu <freewalker.tw@gmail.com>
2014-06-06 12:21:17 +0800

    add migration create devices

---
+++ db/migrate/20140606034721_create_devices.rb
@@ -0,0 +1,16 @@
+class CreateDevices < ActiveRecord::Migration
+  def up
+    create_table :devices do |t|
+      t.string :serical_number, limit: 100
+      t.string :mac_address, limit: 100
+      t.string :model_name, limit: 120
+      t.string :fireware_version, limit: 120
+
+      t.timestamps
+    end
+    add_index :devices, [:serical_number, :mac_address]
+  end
+  def down
+  	drop_table :devices
+  end
+end
