4c5533a4318925eaafae2598586182aeb7ef15f1
travisliu <freewalker.tw@gmail.com>
2014-06-17 12:34:09 +0800

    change migration

--- db/migrate/20140611100658_add_confirmable.rb
+++ db/migrate/20140611100658_add_confirmable.rb
@@ -1,6 +1,6 @@
 class AddConfirmable < ActiveRecord::Migration
 
-  def self.up
+  def self.change
     # add_column :users, :confirmation_token, :string
     # add_column :users, :confirmed_at, :datetime
     # add_column :users, :confirmation_sent_at, :datetime
