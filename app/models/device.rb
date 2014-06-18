5fb7950f46812aa9190ce06d833104c97a8fb597
Yang, Hung <yang.hung.tw@gmail.com>
2014-06-18 10:13:56 +0800

    add mac address validate

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,6 +2,10 @@ class Device < ActiveRecord::Base
 
   has_one :device_session
 
+  before_save { mac_address.downcase! }
+  VALID_MAC_ADDRESS_REGEX = /^([0-9a-f]{2}:){5}[0-9a-f]{2}$/i
+  validates :mac_address, format: { with VALID_MAC_ADDRESS_REGEX }
+
   def self.checkin args
 
   	result = self.where( args.permit(:mac_address, :serial_number))
