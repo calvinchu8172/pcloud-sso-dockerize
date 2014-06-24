f96a090ef43af70574276c0dcdb8a1efb5b09bdf
travisliu <freewalker.tw@gmail.com>
2014-06-24 16:26:31 +0800

    merge to develop

--- app/controllers/discoverer_controller.rb
+++ app/controllers/discoverer_controller.rb
@@ -1,19 +1,9 @@
-<<<<<<< HEAD:app/controllers/pairing_controller.rb
-class PairingController < ApplicationController
-  include PairingHelper
-=======
 class DiscovererController < ApplicationController
-  
->>>>>>> feature/refactore_config_file:app/controllers/discoverer_controller.rb
+  include PairingHelper
   before_action :authenticate_user!
-  before_filter :check_device_avaliable,
+  before_filter :check_device_avaliable, :only => [:check]
 
   def index
-<<<<<<< HEAD:app/controllers/pairing_controller.rb
-  	device_id = params[:id];
-    @session = PairingSession.create(:user_id => current_user.id, :device_id => device_id, :expire_at => (Time.now + (12.minutes)))
-    push_to_queue
-=======
     
     @device_session_list = search_available_device.where(:ip => request.remote_ip)
 
@@ -35,9 +25,6 @@ class DiscovererController < ApplicationController
     end
   end
 
-  def find
-  end
-
   def add
     @device = Device.new
   end
@@ -52,9 +39,6 @@ class DiscovererController < ApplicationController
     end
   end
 
-  def success
-  end
-
   def check
     @device = Device.find(params[:id])
   end
@@ -65,36 +49,5 @@ class DiscovererController < ApplicationController
 
   def search_available_device
     DeviceSession.where("device_id not in (?) AND device_id not in (?)" , PairingSession.handling_by_user(current_user.id).select(:device_id), Pairing.where(:user_id => current_user.id).select(:device_id))
-  end
-
-  def check_device_avaliable
-
-    device_id = params[:id]
-    if device_registered?(device_id) 
-      flash[:alert] = "device not found"
-      redirect_to controller: "pairing", action: "index" 
-    elsif handling?(current_user.id, device_id)
-      flash[:alert] = "device is pairing"
-      redirect_to controller: "pairing", action: "index" 
-    elsif paired?(current_user.id, device_id)
-      flash[:alert] = "device is paired already"
-      redirect_to controller: "pairing", action: "index" 
-    end
-  end
-
-  def device_registered?(device_id)
-    DeviceSession.where(:device_id => device_id).empty?
-  end
-
-  def handling?(user_id, device_id)
-    !PairingSession.handling_by_user(user_id).where(:device_id => device_id).empty?
->>>>>>> feature/refactore_config_file:app/controllers/discoverer_controller.rb
-  end
-
-  def push_to_queue
-  	data = {:job => "pair", :session_id => @session.id}
-  	sqs = AWS::SQS.new
-  	queue = sqs.queues.create()
-  	queue.send_message(data.to_json)
   end
 end
