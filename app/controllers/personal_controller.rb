0c00d0000a45a8022747cbb22f7956dd6a4afbc4
Yang, Hung <yang.hung.tw@gmail.com>
2014-06-27 11:13:41 +0800

    finish unpairing page

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -1,6 +1,6 @@
 class PersonalController < ApplicationController
   before_action :authenticate_user!
-
+  @@unpairing_device = -1 
   def index
     @pairing = Pairing.where(user_id: current_user.id)
   end
@@ -12,5 +12,19 @@ class PersonalController < ApplicationController
   end
 
   def unpairing
+      @pairing = Pairing.find(params[:id])
+      @unpaired_device = false
+      # when pairing  
+      rescue ActiveRecord::RecordNotFound
+        @unpaired_device = true
+        @pairing = Device.find(@@unpairing_device)
   end
+
+  def destroy
+    pairing = Pairing.find(params[:id])
+    @@unpairing_device = pairing.device_id
+    Pairing.find(params[:id]).destroy
+    redirect_to "/personal/unpairing/"
+  end
+
 end
