499fffc2327318aa8bbe9b62b75886758738835b
Yang, Hung <yang.hung.tw@gmail.com>
2014-07-10 15:45:11 +0800

    remove logger

--- config/initializers/client_field_error_proc.rb
+++ config/initializers/client_field_error_proc.rb
@@ -1,10 +1,4 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  Rails.logger.debug "HTML_TAG:"
-  Rails.logger.debug html_tag
-  Rails.logger.debug "Instance:"
-  instance.error_message.each do |i|
-    Rails.logger.debug i
-  end
   if html_tag =~ /th_label/
     if html_tag =~ /checkbox/
       %{#{html_tag}<div class="zyxel_arlert_area"><label for="#{instance.send(:tag_id)}" class="error_message">#{instance.error_message.first}</label></div>}.html_safe
