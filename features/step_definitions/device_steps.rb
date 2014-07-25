e253564236a2673cbae44ae6e356b49b64943ffc
travisliu <freewalker.tw@gmail.com>
2014-07-25 11:14:22 +0800

    add test of device rest api

---
+++ features/step_definitions/device_steps.rb
@@ -0,0 +1,16 @@
+Given(/^the device with the following payload::$/) do |form|
+  @form = form.rows_hash
+end 
+
+When /^the device requests POST (.*)$/ do |path|
+  option = {:method => :post, :params => @form}
+  request path, option
+end
+
+Then(/^response should be "(.*?)"$/) do |status|
+  if self.respond_to? :should
+    last_response.status.should == status.to_i
+  else
+    assert_equal status.to_i, last_response.status
+  end
+end
