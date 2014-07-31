a857267adae1f1399e45bcde36051700c37b0fef
Yang, Hung <yang.hung.tw@gmail.com>
2014-07-31 17:40:35 +0800

    change confirm within day

--- config/initializers/client_field_error_proc.rb
+++ config/initializers/client_field_error_proc.rb
@@ -1,7 +1,7 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   if html_tag =~ /th_label/
     if html_tag =~ /checkbox/
-      %{#{html_tag}<div class="zyxel_arlert_area"><label for="#{instance.send(:tag_id)}" class="error_message">#{instance.error_message.first}</label></div>}.html_safe
+      %{#{html_tag}<label for="#{instance.send(:tag_id)}" class="check_icon"><span></span></label><div class="zyxel_arlert_area"><label for="#{instance.send(:tag_id)}" class="error_message">#{instance.error_message.first}</label></div>}.html_safe
     else
       %{#{html_tag}}.html_safe
     end
