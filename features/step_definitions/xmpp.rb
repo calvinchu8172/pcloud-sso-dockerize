ee9d8c48bd2cff8f0cc1e87ab0fff009738cfecc
Josh <spjay1@gmail.com>
2014-07-31 13:25:35 +0800

    Fix cucumber rest api testing and clean files

--- features/step_definitions/xmpp.rb
+++ features/step_definitions/xmpp.rb
@@ -1,47 +1,39 @@
-Given(/^the device with get::$/) do |table|
-  @table = table.rows_hash
-end
 
-When /^the device request POST (.*)$/ do |path|
-  @hostname = 'http://127.0.0.1:3000'
-  @path = @hostname + path
-  post(@path, @table)
-end
 
-Then(/^I will get http code and (.*)$/) do |json|
-  puts @this_status = last_response.status.to_s
+# Given(/^the device with get::$/) do |table|
+#   @table = table.rows_hash
+# end
 
-  # Backup for document status
-  #
-  # if @this_status[0] == "2"
-  #   puts JSON.parse(last_response.body) 
-  # elsif @this_status[0] == "4" || @this_status[0] == "5"
-  #   puts JSON.parse(last_response.body) 
-  # else
-  #   puts ''
-  # end
+# When /^the device request POST (.*)$/ do |path|
+#   @hostname = 'http://127.0.0.1:3000'
+#   @path = @hostname + path
+#   post(@path, @table)
+# end
 
-  puts @xmpp_table = JSON.parse(last_response.body) 
+# Then(/^I will get http code and (.*)$/) do |json|
+#   puts 'HTTP code: ' + @this_status = last_response.status.to_s
 
-end
+#   @xmpp_table = JSON.parse(last_response.body) 
+#   @xmpp_table = JSON.pretty_generate(@xmpp_table)
 
-When(/^I ack bot$/) do
+#   if @this_status[0] == "2"
+#     puts @xmpp_table 
+#   elsif @this_status[0] == "4" || @this_status[0] == "5"
+#     puts @xmpp_table
+#   else
+#     puts ''
+#   end
 
-  # Backup for dev env 
-  #
-  # puts @username = @xmpp_table["xmpp_account"].gsub('/device', '')
-  # @password = @xmpp_table["xmpp_password"]
-  # puts @bot      = @xmpp_table["xmpp_bots"].to_s.gsub('/robot', '')
+# end
 
-  # for reset password issue
-  puts " It's test env, not real pass data"
-  @username = 'd127001-01@aphpxmpp.pcloud.ecoworkinc.com/device'.gsub('/device', '')
-  @password = 'jSBFVbnKzD'
+Then /I use xmpp_acount to ack bot/ do 
+  @username = @xmpp_acount["xmpp_account"]
+  @password = @xmpp_acount["xmpp_password"]
 
   # @xmpp_table["xmpp_bots"] = ["bot01@aphpxmpp.pcloud.ecoworkinc.com/robot"]
   # clean the hash
   # Ref.http://stackoverflow.com/a/13972656
-  @bot = @xmpp_table["xmpp_bots"]*","
+  @bot = @xmpp_acount["xmpp_bots"]*","
   @bot = @bot.gsub('/robot', '')
 
   Bot.new(@username, @password, @bot).connect
