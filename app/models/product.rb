00f9381dffa52734aa0be45569be8a9430fce302
Josh <spjay1@gmail.com>
2014-08-11 16:41:41 +0800

    [#13983] check file type

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,5 @@ class Product < ActiveRecord::Base
   has_attached_file :pairing
 
   validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :pairing, :content_type => /\Aimage\/.*\Z/
 end
