d99606efc87ec78007f4f2535b33eaa57fb92761
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-08-15 15:32:08 +0800

    Patch deploy/before_migrate.rb
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,8 +1,6 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 execute "bower install" do
-  Chef::Log.info("Node: #{node.inspect}")
-  Chef::Log.info("Release path is: #{release_path}")
-  cwd "#{release_path}"
+  cwd release_path
   command "bower --allow-root install"
 end
