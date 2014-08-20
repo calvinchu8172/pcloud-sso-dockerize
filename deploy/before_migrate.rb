9427c58288d73e4abb2ff2d90f15efcf9b8e8f1c
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-08-20 19:12:14 +0800

    Try to set environment var. SECRET_KEY_BASE
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -5,8 +5,8 @@ execute "bower install" do
   command "bower --allow-root install"
 end
 
-ruby_block "test if we can set environment variables" do
+ruby_block "try to set environment variable SECRET_KEY_BASE" do
   block do
-    ENV['TEST_VAR'] = "test_foo"
+    ENV['SECRET_KEY_BASE'] = "c894834ec3f545ba9f1495e4a68be7db6c25ba3400439349c2e86684845c90fd7a6eccf666cc651c2f1b38a80585c8cf4ed7466a15acdba2b9701402a5af2aef"
   end
 end
