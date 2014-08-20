0a5e58c713f0b7eed90a6d1b82d0fa6f1dcbc55c
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-08-20 19:05:39 +0800

    Test if we can set environment var.
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,3 +4,9 @@ execute "bower install" do
   cwd release_path
   command "bower --allow-root install"
 end
+
+ruby_block "test if we can set environment variables" do
+  block do
+    ENV['TEST_VAR'] = "test_foo"
+  end
+end
