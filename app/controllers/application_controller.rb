e3c5445de4e25b83fc50aafc9847ed2ebea25469
Yang, Hung <yang.hung.tw@gmail.com>
2014-09-19 17:38:24 +0800

    Move the locate setting from application controller to locale.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  include Locale
   before_filter :set_locale
+
   before_filter :setup_log_context
   after_action :store_location
 
@@ -39,43 +42,6 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    # i18n setting
-    def set_locale
-
-      # note: cookie(change before) > user.lang(registered) > browser > default
-      locale = if params[:locale]
-               # Return cookie if user change locale
-                  params[:locale].to_sym if I18n.available_locales.include?(params[:locale].to_sym)
-                  cookies[:locale] = params[:locale]
-               elsif cookies[:locale]
-                  cookies[:locale] if I18n.available_locales.include?(cookies[:locale].to_sym)
-
-               # Return db setting if registered user haven't change before
-               elsif user_signed_in?
-                 current_user.language if I18n.available_locales.include?(current_user.language.to_sym)
-
-               elsif request.env['HTTP_ACCEPT_LANGUAGE']
-                 check_browser_locale(request.env['HTTP_ACCEPT_LANGUAGE'])
-
-               else
-                 I18n.available_locales.first unless locale
-               end
-
-      if locale
-        current_user.change_locale!(locale.to_s) if user_signed_in?
-        I18n.locale = locale
-      end
-
-      # language select option
-      @locale_options = { :English => 'en',
-                          :Deutsch => 'de',
-                          :Nederlands => 'nl',
-                          :"正體中文" => "zh-TW",
-                          :"ไทย" => 'th',
-                          :"Türkçe" => 'tr'}
-    end
-    # i18n setting - end
-
     # Redirect back to current page after sign in
     def store_location
       return unless request.get?
@@ -101,21 +67,5 @@ class ApplicationController < ActionController::Base
 
     def paired?(device_id, user_id)
       Pairing.enabled.exists?({:device_id => device_id, :user_id => user_id})
-    end
-
-    # Split browser locales array and find first support language
-    def check_browser_locale(browser_langs)
-      accept_locales = []
-      browser_langs.split(',').each do |l|
-       l = l.split(';').first
-       i = l.split('-')
-       if 2 == i.size
-         accept_locales << i[0].to_sym
-         i[1].upcase!
-       end
-       l = i.join('-').to_sym
-       accept_locales << l.to_sym
-      end
-      (accept_locales.select { |l| I18n.available_locales.include?(l) }).first
     end
 end
