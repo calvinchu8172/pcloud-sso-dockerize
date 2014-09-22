1cd28dbee9730c728e3e6b4adaf4495f2102a650
Josh <spjay1@gmail.com>
2014-09-22 18:04:32 +0800

    Fix i18n application default value & remove to_sym

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,20 +45,20 @@ class ApplicationController < ActionController::Base
       # note: cookie(change before) > user.lang(registered) > browser > default
       locale = if params[:locale]
                # Return cookie if user change locale
-                  params[:locale].to_sym if I18n.available_locales.include?(params[:locale].to_sym)
+                  params[:locale] if I18n.available_locales.include?(params[:locale])
                   cookies[:locale] = params[:locale]
                elsif cookies[:locale]
-                  cookies[:locale] if I18n.available_locales.include?(cookies[:locale].to_sym)
+                  cookies[:locale] if I18n.available_locales.include?(cookies[:locale])
 
                # Return db setting if registered user haven't change before
                elsif user_signed_in?
-                 current_user.language if I18n.available_locales.include?(current_user.language.to_sym)
+                 current_user.language if I18n.available_locales.include?(current_user.language)
 
                elsif request.env['HTTP_ACCEPT_LANGUAGE']
                  check_browser_locale(request.env['HTTP_ACCEPT_LANGUAGE'])
 
                else
-                 I18n.available_locales.first unless locale
+                 I18n.default_locale
                end
 
       if locale
