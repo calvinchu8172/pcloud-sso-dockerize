46e7388cda1cf1249e5ddc1284fcec1114dfafae
Josh <spjay1@gmail.com>
2014-09-22 10:45:38 +0800

    fix facebook oauth typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, APP_CONFIG['facebook_app_id'], APP_CONFIG['facebook_secret'],
     :auth_type => 'reauthenticate'
-  config.omniauth :google_oauth2, APP_CONFIG['google_app_id'], APP_CONFIG['google_secret'],
+  config.omniauth :google_oauth2, APP_CONFIG['google_app_id'], APP_CONFIG['google_secret']
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
