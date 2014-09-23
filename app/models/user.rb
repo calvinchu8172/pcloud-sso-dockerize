ef740e4d03a41dcfcaa3ef5354c555bc1d8d81ac
Josh <spjay1@gmail.com>
2014-09-23 13:49:43 +0800

    Fix user model for password & imput length limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   before_create :add_default_display_name
   has_many :identity
 
-  attr_accessor :input
-  validate :input, length: {maximum:255}
+  validates_length_of :email, :first_name, :middle_name, :last_name, :mobile_number, maximum:255
+  validates_length_of :password, in: 8..14
   validates_length_of :mobile_number, maximum: 40
 
   # Include default devise modules. Others available are:
