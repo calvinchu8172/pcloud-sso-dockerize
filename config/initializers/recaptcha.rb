8289fc328a4d8d4037a70acab2685848aa1bfdcd
Travis Liu <travisliu@ecoworkinc.com>
2014-10-01 17:38:58 +0800

    fix recapcha config error

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,6 @@
 Recaptcha.configure do |config|
   # config.public_key  = '6LelPvUSAAAAAHQP8AccRDnMO0ETmgmtY-okFr95'
   # config.private_key = '6LelPvUSAAAAAGdYfFVdxTPayPH1cQG5jIpoDqcj'
-  config = Settings.recaptcha.config
+  config.public_key = Settings.recaptcha.config.public_key
+  config.private_key = Settings.recaptcha.config.private_key
 end
