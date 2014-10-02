b54732eaf6e1a4a6e9907590b602050f273256ba
Travis Liu <travisliu@ecoworkinc.com>
2014-10-02 15:08:10 +0800

    add
    * xmpp server list in restful api
    * xmpp session on redis
    * rails_admin config in stagin env
    * modify xmpp acount to mogoosim db

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -16,23 +16,35 @@ class DeviceController < ApplicationController
   skip_before_filter :verify_authenticity_token
   skip_before_filter :authenticate_user!
   before_filter :validate_signature, :only => :register
+  before_filter :verify_device, :only => :register
 
   # POST /d/1/register
   def register
 
-    @device = Device.checkin(api_permit)
+    # @device = Device.checkin(api_permit)
+    device_checkin
     device_session_checkin
     ddns_checkin
     reset
 
-    xmpp_account = xmpp_checkin
+    xmpp_checkin
 
-  	render :json => {:xmpp_account => xmpp_account[:name] + '@' + Settings.xmpp.server + "/" + Settings.xmpp.device_rescource_id,
-                     :xmpp_password => xmpp_account[:password],
-                     :xmpp_bots => Settings.xmpp.bots}
+  	render :json => {:xmpp_account => @account[:name] + '@' + Settings.xmpp.server + "/" + Settings.xmpp.device_rescource_id,
+                     :xmpp_password => @account[:password],
+                     :xmpp_bots => Settings.xmpp.bots,
+                     :xmpp_ip_addresses => Settings.xmpp.nodes}
   end
 
   private
+
+  def device_checkin
+
+    unless api_permit[:firmware_version] == @device.firmware_version
+      logger.info('update device from fireware version' + args[:firmware_version] + ' from ' + @device.firmware_version)
+      @device.update_attribute(:firmware_version, args[:firmware_version])
+    end
+
+  end
 
   def reset
     unless reset_requestment?
@@ -93,20 +105,21 @@ class DeviceController < ApplicationController
     admin_username = Settings.xmpp.admin.account
     admin_password = Settings.xmpp.admin.password
 
-    account = Hash.new
-    account[:password] = generate_new_passoword
-    account[:name] = @device.session.fetch :xmpp_account || generate_new_username
+    @account = Hash.new
+    @account[:password] = generate_new_passoword
+    @account[:name] = @device.session.fetch :xmpp_account || generate_new_username
 
-    if(@device.device_session.nil?)
-      # connect_to_xmpp(admin_username + "@" + xmpp_host, admin_password.to_s)
-      @account = {:name => generate_new_username, :password => generate_new_passoword}
-      logger.info('create new xmpp account:' + @account[:name])
-    else
-      # connect_to_xmpp(@device.device_session.xmpp_account + "@" + xmpp_host, @device.device_session.password)
-      @account = {:name => @device.device_session.xmpp_account, :password => generate_new_passoword}
-      logger.info('change password for account:' + @account[:name])
-    end
+    # if(@device.device_session.nil?)
+    #   # connect_to_xmpp(admin_username + "@" + xmpp_host, admin_password.to_s)
+    #   @account = {:name => generate_new_username, :password => generate_new_passoword}
+    #   logger.info('create new xmpp account:' + @account[:name])
+    # else
+    #   # connect_to_xmpp(@device.device_session.xmpp_account + "@" + xmpp_host, @device.device_session.password)
+    #   @account = {:name => @device.device_session.xmpp_account, :password => generate_new_passoword}
+    #   logger.info('change password for account:' + @account[:name])
+    # end
 
+    logger.info('apply xmpp account:' + @account[:name])
     apply_for_xmpp_account
   end
 
@@ -125,9 +138,12 @@ class DeviceController < ApplicationController
   end
 
   def apply_for_xmpp_account
+
     xmpp_user = XmppUser.find_or_initialize_by(username: @account[:name])
     xmpp_user.password = @account[:password]
     xmpp_user.save
+
+    xmpp_user.session = @device.id
   end
 
   # 用Mac Address 和 序號的英數產生
@@ -138,6 +154,28 @@ class DeviceController < ApplicationController
   def generate_new_passoword
     origin = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
     (0...10).map { origin[rand(origin.length)] }.join
+  end
+
+  def verify_device
+
+    args = api_permit
+    result = Device.where( args.slice(:mac_address, :serial_number))
+
+    if result.empty?
+
+      product = Product.where(args.slice(:model_name))
+      logger.debug('product search result:' + product.inspect);
+      unless product.empty?
+        @device = Device.create(args.slice(:mac_address, :serial_number, :firmware_version).merge(product_id: product.first.id))
+        logger.info('create new device id:' + @device.id.to_s)
+        return 
+      end
+
+      logger.info('result: invalid parameter');
+      render :json => {:result => 'invalid parameter'}, :status => 400
+    else
+      @device = result.first
+    end
   end
 
   def validate_signature
