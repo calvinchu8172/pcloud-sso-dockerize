9c030942af222bd537b771ef12904466133fdc23
Yang, Hung <yang.hung.tw@gmail.com>
2014-10-07 10:44:32 +0800

    Comment the ssl config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  
+
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
