1e506986251370f49a19c960a19b0a5576d8e41a
Josh Wang <spjay1@gmail.com>
2014-12-03 10:58:51 +0800

    Remove un-use code(get_browser_locale)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -66,20 +66,4 @@ class ApplicationController < ActionController::Base
       queue = sqs.queues.named(Settings.environments.sqs.name)
       queue.send_message(data.to_json)
     end
-
-    # Split browser locales array and find first support language
-    def get_browser_locale(browser_langs)
-      accept_locales = []
-      browser_langs.split(',').each do |l|
-       l = l.split(';').first
-       i = l.split('-')
-       if 2 == i.size
-         accept_locales << i[0].to_sym
-         i[1].upcase!
-       end
-       l = i.join('-').to_sym
-       accept_locales << l.to_sym
-      end
-      (accept_locales.select { |l| I18n.available_locales.include?(l) }).first
-    end
 end
