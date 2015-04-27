1b8345015e61e6eda481671db5704cf924ec2935
Ryan <ryan@LiYu-KuangdeMacBook-Pro.local>
2015-04-27 12:27:20 +0800

    change search behavior with cucumber test

--- app/models/device.rb
+++ app/models/device.rb
@@ -123,10 +123,6 @@ class Device < ActiveRecord::Base
     false
   end
 
-  def serial_number_verified?(serial_number)
-    self.serial_number == serial_number
-  end
-
   def dont_verify_serial_number?
     ['NSA325', 'NSA325 v2'].include?(self.product.model_class_name)
   end  
@@ -136,7 +132,7 @@ class Device < ActiveRecord::Base
     return if devices.empty?
     device = devices.first
     return device if device.dont_verify_serial_number? 
-    return device if device.serial_number_verified?(serial_number)
+    return device if device.serial_number == serial_number
   end
 
 end
