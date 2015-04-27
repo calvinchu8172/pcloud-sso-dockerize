eb39239e715f7ce031cdec7671e211c516feded8
Ryan <ryan@LiYu-KuangdeMacBook-Pro.local>
2015-04-27 17:07:07 +0800

    [#15481] fix NSA325v2 and NSA325 Serial number issue * device.rb: simplify the code of search method ref #15481 @0.5 hours

--- app/models/device.rb
+++ app/models/device.rb
@@ -130,12 +130,9 @@ class Device < ActiveRecord::Base
   def self.search(mac_address, serial_number)
     devices = Device.where(mac_address: mac_address)
     return if devices.empty?
-    device = nil
-    devices.each do |d|
-         device = d if d.serial_number == serial_number
+    devices.each do |device|
+      return device if device.dont_verify_serial_number?
+      return device if device.serial_number == serial_number
     end
-    return device unless device.blank?
-    return device if device.dont_verify_serial_number? 
   end
-
 end
