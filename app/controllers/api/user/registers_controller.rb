1efaf640c153499b30d47ded9f7d24aa000319e2
Travis Liu <travisliu@ecoworkinc.com>
2015-05-14 21:59:35 +0800

    remove unnecessary error

--- app/controllers/api/user/registers_controller.rb
+++ app/controllers/api/user/registers_controller.rb
@@ -12,8 +12,6 @@ class Api::User::RegistersController < Api::Base
        "002" => "password",
        "003" => "certificaate",
        "004" => "signature"}.each { |error_code, field| return render :json =>  {error_code: error_code, description: @register.errors[field].first} unless @register.errors[field].empty?}
-      
-      return render :json =>  {error_code: "999", description: @register.errors} unless @register.errors.empty?
     end
 
     @user.app_info.bulk_set(token_params.slice(:app_key, :os)) if !token_params[:app_key].blank? and !token_params[:os].blank? and ['1', '2'].include?(token_params[:os])
