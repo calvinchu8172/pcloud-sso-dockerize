04fc694b7c14ba11d35ac5ac13553675d11437c8
Travis Liu <travisliu@ecoworkinc.com>
2015-05-18 22:21:29 +0800

    add certificatae validator

--- app/validators/ssl_validator.rb
+++ app/validators/ssl_validator.rb
@@ -1,22 +1,37 @@
 class SslValidator < ActiveModel::Validator
+
   def validate(record)
 
-  	key = ""
-    options[:signature_key].each {|field| key += record.send(field.to_s)}
-
-    # certificate = OpenSSL::X509::Certificate.new(record.certificate)
-    # public_key = OpenSSL::PKey::RSA.new(ENV['AUTH_PUBLIC_KEY'])
-
-    # certificate.verify(public_key)
-    # validate_signature(record.certificate, key)
-    record.errors["certificaate"] = "invalid certificate" unless record.certificate == "1"
-    record.errors["signature"] = "invalid signature" unless record.signature == "1"
+    # key = options[:signature_key].inject {|result, element| result += record.send(element.to_s)}
+    key = options[:signature_key].map{|field| record.send(field.to_s)}.join('')
+    
+    record.errors["certificaate"] = key unless verify_certificate(record.certificate) #record.certificate == "1"
+    record.errors["signature"] = "invalid signature" unless validate_signature(record.signature, key) #record.signature == "1"
   end
 
-   def validate_signature(signature, key)
+  def validate_signature(signature, key)
     sha224 = OpenSSL::Digest::SHA224.new
-    signature_inside = sha224.hexdigest(key)
+    public_key.verify(sha224, signature, key)
 
-    signature == signature_inside
+    signature == "1"
   end
+
+  private
+
+    def verify_certificate certificate
+      begin
+        certificate = OpenSSL::X509::Certificate.new(CGI::unescape(certificate))
+        return certificate.verify(public_key)
+      rescue
+        return false
+      end
+    end  
+
+    def public_key
+
+      return @public_key unless @public_key.blank?
+
+      key_string = ENV['PUBLIC_KEY'] || Settings.environments.public_key
+      @public_key = OpenSSL::PKey::RSA.new(key_string)
+    end
 end
