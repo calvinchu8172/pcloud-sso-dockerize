fc822918c5f679dc9aa945191b5fc79310af26c0
Travis Liu <travisliu@ecoworkinc.com>
2015-05-20 22:54:43 +0800

    add create action

--- app/controllers/api/user/confirmation_controller.rb
+++ app/controllers/api/user/confirmation_controller.rb
@@ -1,4 +1,13 @@
 class Api::User::ConfirmationController < ApplicationController
   def create
+
+  	user = User.send_confirmation_instructions(email: valid_params[:email])
+  	return render json: {result: 'success'} if user.errors.empty?
+  	render json: {code: 001, description: 'E-mail not found.'}
   end
+
+  private 
+    def valid_params
+      params.permit(:email)
+    end
 end
