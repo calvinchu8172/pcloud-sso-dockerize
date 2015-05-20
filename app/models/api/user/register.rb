d38f736e89c8b20df7fd6a63362c7ef8a4be187f
Travis Liu <travisliu@ecoworkinc.com>
2015-05-20 22:29:02 +0800

    chagen certificate field to certificate_serial

--- app/models/api/user/register.rb
+++ app/models/api/user/register.rb
@@ -1,6 +1,6 @@
 class Api::User::Register < Api::User
-  attr_accessor :certificate, :signature
-  validates_with SslValidator, signature_key: [:email, :password, :certificate]
-  validates_presence_of :email, :password, :certificate, :signature, message: "invalid parameters"
+  attr_accessor :certificate_serial, :signature
+  validates_with SslValidator, signature_key: [:email, :password, :certificate_serial]
+  validates_presence_of :email, :password, :certificate_serial, :signature, message: "invalid parameters"
 
 end
