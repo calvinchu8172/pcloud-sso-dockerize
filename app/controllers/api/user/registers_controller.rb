6cbefcdd9bf276d0a57713560de7e2c243311cf8
Travis Liu <travisliu@ecoworkinc.com>
2015-05-20 22:27:19 +0800

    remove sign in

--- app/controllers/api/user/registers_controller.rb
+++ app/controllers/api/user/registers_controller.rb
@@ -1,8 +1,7 @@
 class Api::User::RegistersController < Api::Base
 
   def create
-    key_string = ENV['PUBLIC_KEY'] || Settings.environments.public_key
-    logger.debug('public key:' + key_string)
+    
     @user = Api::User::Register.new valid_params.except(:id)
     @user.email = valid_params[:id]
     @user.agreement = "1"
@@ -16,7 +15,7 @@ class Api::User::RegistersController < Api::Base
        "004" => "signature"}.each { |error_code, field| return render :json =>  {error_code: error_code, description: @user.errors[field].first} unless @user.errors[field].empty?}
     end
 
-    sign_in(:user, @user, store: false, bypass: false)
+    
     @user.app_info.bulk_set(token_params.slice(:app_key, :os)) if !valid_params[:app_key].blank? and !valid_params[:os].blank? and ['1', '2'].include?(valid_params[:os])
     @user.create_token_set
 
