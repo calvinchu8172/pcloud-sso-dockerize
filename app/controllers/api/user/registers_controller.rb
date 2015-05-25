67f348ff032cf62e6deb5c32eb58906134f80db7
Travis Liu <travisliu@ecoworkinc.com>
2015-05-25 20:01:40 +0800

    added invalid parameters error code 000

--- app/controllers/api/user/registers_controller.rb
+++ app/controllers/api/user/registers_controller.rb
@@ -6,17 +6,18 @@ class Api::User::RegistersController < Api::Base
     @user.email = valid_params[:id]
     @user.agreement = "1"
 
-    logger.debug "certificate_validator record:" + valid_params.inspect
-
     unless @user.save
+
+      logger.debug('user errors:' + @user.errors.inspect)
+
       {"001" => "email",
        "002" => "password",
        "003" => "certificaate",
-       "004" => "signature"}.each { |error_code, field| return render :json =>  {error_code: error_code, description: @user.errors[field].first} unless @user.errors[field].empty?}
+       "004" => "signature"}.each { |error_code, field| return render :json =>  {error_code: error_code, description: field + @user.errors[field].first} unless @user.errors[field].empty?}
+
+       return render :json =>  {error_code: '000', description: 'invalid parameters'} unless @user.errors.empty?
     end
 
-    
-    @user.app_info.bulk_set(token_params.slice(:app_key, :os)) if !valid_params[:app_key].blank? and !valid_params[:os].blank? and ['1', '2'].include?(valid_params[:os])
     @user.create_token_set
 
   	render "api/user/tokens/create.json.jbuilder"
