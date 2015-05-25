e6b64830c03995caa367de996c3c28f4c2e18ac0
Travis Liu <travisliu@ecoworkinc.com>
2015-05-25 18:43:04 +0800

    added app_key and os validator

--- app/models/api/user/register.rb
+++ app/models/api/user/register.rb
@@ -1,6 +1,14 @@
 class Api::User::Register < Api::User
-  attr_accessor :certificate_serial, :signature
+  attr_accessor :certificate_serial, :signature, :app_key, :os
   validates_with SslValidator, signature_key: [:email, :password, :certificate_serial]
   validates_presence_of :email, :password, :certificate_serial, :signature, message: "invalid parameters"
+  validates_presence_of :app_key, if: :app_key_os_need_validate?
+  validates :os, inclusion: { in: %w(0 1 2),
+    message: "invalid parameters" },
+    if: :app_key_os_need_validate?
 
+
+  def app_key_os_need_validate?
+  	!os.blank? or !app_key.blank?
+  end
 end
