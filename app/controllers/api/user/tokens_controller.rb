07a7667288393924e0363cc3cd12c3f60b293566
Travis Liu <travisliu@ecoworkinc.com>
2015-05-27 00:57:08 +0800

    fixed unconfirm bug

--- app/controllers/api/user/tokens_controller.rb
+++ app/controllers/api/user/tokens_controller.rb
@@ -1,13 +1,11 @@
 # refer to http://fancypixel.github.io/blog/2015/01/28/react-plus-flux-backed-by-rails-api/
-class Api::User::TokenController < Api::Base
+class Api::User::TokensController < Api::Base
   
   def create
     
     unless @user = Api::User::Token.authenticate(token_params)
-      return  render json: {code: '001', description: 'Invalid email or password.'}
+      return  render json: @user.errors[:authenticate].first
     end
-
-    return  render json: {code: '002', description: 'client have to confirm email account before continuing.'} unless @user.confirmed?
     
     @user.app_info.bulk_set(token_params.slice(:app_key, :os)) if !token_params[:app_key].blank? and !token_params[:os].blank? and ['1', '2'].include?(token_params[:os])
     @user.create_token_set
