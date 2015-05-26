1e7db38e0157dc0b7b1bd18443c279d0ecfb8c1d
Travis Liu <travisliu@ecoworkinc.com>
2015-05-27 00:57:47 +0800

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Rails.application.routes.draw do
 
     scope :path => '/user/1/', :module => "api/user" do
       # match ':controller(/:action(/:id(.:format)))', :via => :all
-      resource :tokens, format: 'json'
+      resource :token, format: 'json'
       resource :register, format: 'json'
     end
     
