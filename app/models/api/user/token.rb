edcb69e49ad4df62a61f14a39279d85f14337e88
Travis Liu <travisliu@ecoworkinc.com>
2015-05-27 00:57:37 +0800

    refactored error message

--- app/models/api/user/token.rb
+++ app/models/api/user/token.rb
@@ -3,8 +3,15 @@ class Api::User::Token < Api::User
   def self.authenticate(payload = {})
 
     user = self.find_for_database_authentication(email: payload[:email])
-    return unless user
-    return unless user.valid_password?(payload[:password])
+    unless user and user.valid_password?(payload[:password])
+      errors.add(:authenticate, {code: '001', description: 'Invalid email or password.'})
+      return false
+    end
+    
+    if ((DateTime.now - user.created_at) / 1.day) > 3
+      errors.add(:authenticate, {code: '002', description: 'client have to confirm email account before continuing.'})
+      return false
+    end
 
     user
   end
