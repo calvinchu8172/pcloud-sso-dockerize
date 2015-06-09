4bce4fc895e42a654986129d12d4fab26c446123
Travis Liu <travisliu@ecoworkinc.com>
2015-06-09 14:50:52 +0800

    remove module payload

--- app/controllers/api/device_controller.rb
+++ app/controllers/api/device_controller.rb
@@ -12,7 +12,7 @@ class Api::DeviceController < Api::Base
 
   private
     def api_permit
-      params.permit(:mac_address, :serial_number, :model_class_name, :firmware_version, :signature, :module, :algo, :reset)
+      params.permit(:mac_address, :serial_number, :model_class_name, :firmware_version, :signature, :algo, :reset)
     end
 
     def device_checking
