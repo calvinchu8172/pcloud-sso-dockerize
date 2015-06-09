92042070de43064d515d3f6a03f0caaa768a3a56
Travis Liu <travisliu@ecoworkinc.com>
2015-06-09 11:41:28 +0800

    add device api v1 & v3

--- app/controllers/api/device_controller.rb
+++ app/controllers/api/device_controller.rb
@@ -1,3 +1,22 @@
 class Api::DeviceController < Api::Base
-  
+
+  def create
+
+    @device.ddns_checkin
+    @device.install_module
+    @device.device_session_checkin
+    @device.reset_pairing
+
+    @device.xmpp_checkin
+  end
+
+  private
+    def api_permit
+      params.permit(:mac_address, :serial_number, :model_class_name, :firmware_version, :signature, :module, :algo, :reset)
+    end
+
+    def device_checking
+      @device = Api::Device.checkin(api_permit.merge(current_ip_address: request.remote_ip))
+      return render :json => {:result => 'invalid parameter'}, :status => 400 if @device.nil?
+    end
 end
