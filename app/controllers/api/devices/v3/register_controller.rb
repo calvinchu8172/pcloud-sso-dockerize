d49147b7bac566f491ee024f0a2e1d738be1cfbd
Travis Liu <travisliu@ecoworkinc.com>
2015-06-10 22:49:28 +0800

    improve error code

--- app/controllers/api/devices/v3/register_controller.rb
+++ app/controllers/api/devices/v3/register_controller.rb
@@ -9,9 +9,9 @@ class Api::Devices::V3::RegisterController < Api::DeviceController
     def device_checking
       payload = api_permit
       payload[:model_class_name] = payload.delete(:model_name)
-      @device = Api::Device.checkin(api_permit.merge(current_ip_address: request.remote_ip))
+      @device = Api::Device::V3.checkin(api_permit.merge(current_ip_address: request.remote_ip))
       return render :json => {:result => 'invalid parameter'}, :status => 400 if @device.nil?
       @device.valid?
-      return render json: @device.errors['signature'].first unless @device.errors['signature'].blank?
+      return render json: @device.errors.values[0].first, :status => 400  unless @device.errors.blank?
     end
 end
