d45a4b28aab5cd53b04848ff996e0bd07a64cab7
Tomo Hung <tomohung@ecoworkinc.com>
2015-06-22 05:04:46 +0000

    fix typo

--- app/controllers/api/user/confirmations_controller.rb
+++ app/controllers/api/user/confirmations_controller.rb
@@ -3,10 +3,10 @@ class Api::User::ConfirmationsController < Api::Base
 
     user = User.send_confirmation_instructions(email: valid_params[:email], 'Content-Transfer-Encoding' => 'UTF-8')
     return render json: {result: 'success'} if user.errors.empty?
-    render json: {error_code: 001, description: 'E-mail ' + user.errors[:email].first}, :status => 400
+    render json: {error_code: "001", description: 'E-mail ' + user.errors[:email].first}, :status => 400
   end
 
-  private 
+  private
     def valid_params
       params.permit(:email)
     end
