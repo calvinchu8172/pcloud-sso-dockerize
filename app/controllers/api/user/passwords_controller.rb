97f471af26660941b0612eeeef94b8b3288a2dea
Tomo Hung <tomohung@ecoworkinc.com>
2015-06-22 04:30:15 +0000

    fix typo

--- app/controllers/api/user/passwords_controller.rb
+++ app/controllers/api/user/passwords_controller.rb
@@ -1,13 +1,13 @@
-class Api::User::PasswordsController < Api::Base  
+class Api::User::PasswordsController < Api::Base
   def create
-  	user = User.send_reset_password_instructions(email: valid_params[:email])
+    user = User.send_reset_password_instructions(email: valid_params[:email])
 
   	return render json: {result: 'success'} if user.errors.empty?
 
-  	render json: {error_code: 001, description: 'E-mail not found.'}, :status => 400
+  	render json: {error_code: "001", description: 'E-mail not found.'}, :status => 400
   end
 
-  private 
+  private
     def valid_params
       params.permit(:email)
     end
