e732dd068cc3b115df2ffb0d57d80f9b3b4ee49d
Josh Wang <spjay1@gmail.com>
2015-06-26 15:20:53 +0800

    [#16096] Fix oauth issue when the same user regist diff identity * fix for stable. ref #16096 @1.5hours

--- app/models/api/identity.rb
+++ app/models/api/identity.rb
@@ -1,4 +1,4 @@
-class Api::User::Identity < Identity
+class Api::Identity < Identity
   attr_accessor :certificate_serial, :signature, :user_id, :access_token
   validates_with SslValidator, signature_key: [:certificate_serial, :user_id, :access_token]
 end
