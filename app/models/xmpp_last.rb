c4b5574d4c8dce1521c71f286a8aad820347ed2a
Tomo Hung <tomohung@ecoworkinc.com>
2015-07-20 17:16:21 +0800

    [#15573] Domain name expire
    * add model: XmppLast
    * add alias name "last_signout_at" for XmppLast::seconds
    * add test for check if XmppLast is created or not
    
    ref #15573 @2.hours

---
+++ app/models/xmpp_last.rb
@@ -0,0 +1,19 @@
+class XmppLast < ActiveRecord::Base
+  establish_connection "xmpp_#{Rails.env}".to_sym
+  alias_attribute :last_signout_at, :seconds
+
+  # CREATE TABLE `last` (
+  #   `username` varchar(250) NOT NULL,
+  #   `seconds` int(11) NOT NULL,
+  #   `state` text NOT NULL,
+  #   `last_signin_at` int(11),
+  #   PRIMARY KEY (`username`)
+  # ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+  self.table_name = "last"
+
+  def online?
+    last_signin_at > last_signout_at
+  end
+
+end
