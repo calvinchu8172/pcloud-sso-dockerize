e37d4a620e490dce7acbffedcc606c86d988c1d1
Ryan <ryan@LiYu-KuangdeMacBook-Pro.local>
2015-07-22 11:24:29 +0800

    [#16333] add validations to resend email API * add validation of email not found * add validation of user confirmed ref #16333 @1.hour

--- app/controllers/api/user/confirmations_controller.rb
+++ app/controllers/api/user/confirmations_controller.rb
@@ -1,9 +1,13 @@
 class Api::User::ConfirmationsController < Api::Base
+
   def create
+    user = Api::User.find_by(email: valid_params[:email])
+    return render json: { error_code: '001', description: 'email not found.' } if user.nil?
+    return render json: { error_code: '006', description: 'the user has been confirmed.' }, :status => 400 if user.confirmed?
 
     user = User.send_confirmation_instructions(email: valid_params[:email], 'Content-Transfer-Encoding' => 'UTF-8')
     return render json: {result: 'success'} if user.errors.empty?
-    render json: {error_code: "001", description: 'E-mail ' + user.errors[:email].first}, :status => 400
+    render json: {error_code: "000", description: 'E-mail ' + user.errors[:email].first}, :status => 400
   end
 
   private
