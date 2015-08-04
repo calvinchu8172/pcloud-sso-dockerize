54cc733bc9382f5dd0261004c7be47eb1a3d39db
Tomo Hung <tomohung@ecoworkinc.com>
2015-08-04 13:28:44 +0800

    overridden confiramtion/show to prevent current_user.nil?

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
-  before_action :user_login!, only: [:edit, :new, :create, :update, :show]
+  before_action :user_login!, only: [:edit, :new, :create, :update]
   before_action :email_validate, only: [:update]
 
   def create
@@ -15,6 +15,19 @@ class ConfirmationsController < Devise::ConfirmationsController
     current_user.send_confirmation_instructions
     sign_out(current_user)
     redirect_to hint_confirm_sent_path
+  end
+
+  def show # overridden
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_flashing_format?
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      redirect_to unauthenticated_root_path
+      # respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
   end
 
   protected
