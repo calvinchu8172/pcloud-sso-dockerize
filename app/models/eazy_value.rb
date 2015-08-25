7702a75949baed656f5662189fc9e4cca92d87ad
colma <colma@ecoworkinc.com>
2015-08-25 15:49:11 +0800

    [#16815] eazy config values * add new model to config app id (etc) * set values in redis client to update web-site settings * set default value in model
    ref #16815 @1hour

---
+++ app/models/eazy_value.rb
@@ -0,0 +1,22 @@
+class EazyValue
+  include Redis::Objects
+  SESSION_INDEX = 'ez:portal:index'
+  @default_value = {}
+  @default_value[:apple_app_id] = '770537600'; # apple app id
+  @default_value[:android_app_id] = 'com.zyxel.zCloud'; # android app id
+  @default_value[:android_app_argument] = 'http://www.zyxel.com/'; #android app reference website
+  @default_value[:android_app_scheme] = 'zyxeldrive://xxx.xxx.xxx'; #android app hook scheme
+  @default_value[:android_app_icon] = 'https://lh4.ggpht.com/ aWuemfRx8lvlluW7pY6eNeV_l0_oxTHNX8r8LFfCAKWGlVox3Y4PCevAhReQH7bL0A=w300-rw'; #android smart banner icon
+
+  def self.find id
+    puts @default_value[id]
+    if  ( @default_value.has_key?(id) == false )
+      return ''
+    end
+    redis_values = Redis::HashKey.new(SESSION_INDEX)
+    if( redis_values.has_key?(id) == false )
+      return @default_value[id]
+    end 
+    redis_values[id]
+  end
+end
