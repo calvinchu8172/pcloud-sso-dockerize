ae1f0b949c7c9829f76a05fb782a26f37476ee32
colma <colma@ecoworkinc.com>
2015-08-26 10:39:30 +0800

    [#16815] add version number in redis key of secondary config * to ensure system will use default value after depoly * to record each temp updates
    ref #16815 @0.5hours

--- app/models/secondary_config.rb
+++ app/models/secondary_config.rb
@@ -1,6 +1,6 @@
 class SecondaryConfig
   #include Redis::Objects
-  @redis_key = 'secondary_config:portal:index'
+  @redis_key = 'secondary_config:portal:' + Version['version']
   @default_value = {}
   @default_value[:apple_app_id] = '770537600'; # apple app id
   @default_value[:android_app_id] = 'com.zyxel.zCloud'; # android app id
@@ -9,7 +9,6 @@ class SecondaryConfig
   @default_value[:android_app_icon] = 'https://lh4.ggpht.com/aWuemfRx8lvlluW7pY6eNeV_l0_oxTHNX8r8LFfCAKWGlVox3Y4PCevAhReQH7bL0A=w300-rw'; #android smart banner icon
 
   def self.find id
-    puts @default_value[id]
     if  ( @default_value.has_key?(id) == false )
       return ''
     end
