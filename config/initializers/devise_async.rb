abe77cd35b4a927494c853ad5fc2e7b44f2306c3
Tomo Hung <tomohung@ecoworkinc.com>
2015-09-01 09:14:41 +0000

    when running test, turn off devise-async

--- config/initializers/devise_async.rb
+++ config/initializers/devise_async.rb
@@ -1,7 +1,7 @@
 # Supported options: :resque, :sidekiq, :delayed_job, :queue_classic, :torquebox, :backburner, :que, :sucker_punch
 Devise::Async.backend = :sidekiq
 Devise::Async.setup do |config|
-  config.enabled = true
+  config.enabled = Rails.env == 'test' ? false : true
   config.backend = :sidekiq
   config.queue = :mailer
 end
