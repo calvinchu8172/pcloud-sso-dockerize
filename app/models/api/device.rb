41120ceb5d32e6f7b21ccf5a8bb9114853b802cc
Hayden Wang <haydenwang@ecoworkinc.com>
2015-10-16 03:49:06 +0000

    Simplify code syntax.

--- app/models/api/device.rb
+++ app/models/api/device.rb
@@ -11,13 +11,12 @@ class Api::Device < Device
     if instance.blank?
       # self.create!(instance.attributes.merge({product_id: @product.id}))
       self.product_id = @product.id
-      self.ip_address = IPAddr.new(current_ip_address).to_i.to_s(16).rjust(8, "0")
+      self.ip_address = ip_encode_hex
       self.save
       logger.info('create new device id:' + self.id.to_s)
       return true
     else
-      self.ip_address = IPAddr.new(current_ip_address).to_i.to_s(16).rjust(8, "0")
-      instance.update_attribute(:ip_address, self.ip_address)
+      instance.update_attribute(:ip_address, ip_encode_hex)
     end
 
     unless firmware_version == instance.firmware_version
@@ -179,5 +178,9 @@ class Api::Device < Device
       logger.debug("validate model name: " + @product.inspect)
 
       errors.add(:parameter, {result: 'invalid parameter'}) if @product.blank?
+    end
+
+    def ip_encode_hex
+      IPAddr.new(current_ip_address).to_i.to_s(16).rjust(8, "0")
     end
 end
