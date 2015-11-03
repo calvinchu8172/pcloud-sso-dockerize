eb2c1caa70bf76d7a5ca9a2cfee839b966df076b
Calvin Chu <calvinchu@ecoworkinc.com>
2015-11-03 11:30:03 +0800

    [#18022] 記錄使用者pairing, unpairing, device reset歷程
    * add column: user_id, device_id, ip_address, status.
    
    ref #18022

--- db/migrate/20151103031121_create_pairing_logs.rb
+++ db/migrate/20151103031121_create_pairing_logs.rb
@@ -1,6 +1,10 @@
 class CreatePairingLogs < ActiveRecord::Migration
   def change
     create_table :pairing_logs do |t|
+      t.integer :user_id
+      t.integer :device_id
+      t.string :ip_address
+      t.integer :status
 
       t.timestamps null: false
     end
