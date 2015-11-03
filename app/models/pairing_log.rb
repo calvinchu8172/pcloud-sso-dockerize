ce4928289d46df0fee2a686c82710322cf53217e
Calvin Chu <calvinchu@ecoworkinc.com>
2015-11-03 19:09:45 +0800

    [#18022] 記錄使用者pairing, unpairing, device reset歷程
    * record when pairing, unpairing and device reset.
    * when pair, status = 1; when unpair, status = 2; when device reset, status = 3
    
    ref #18022

--- app/models/pairing_log.rb
+++ app/models/pairing_log.rb
@@ -1,2 +1,14 @@
 class PairingLog < ActiveRecord::Base
+
+  enum status: { pair: 1, unpair: 2,  device_reset: 3 }
+
+  def self.record_pairing_log(device_id, user_id, ip_address, status)
+    pairing_log = self.new
+    pairing_log.user_id = user_id
+    pairing_log.device_id = device_id
+    pairing_log.ip_address = ip_address
+    pairing_log.status = status
+    pairing_log.save
+  end
+
 end
