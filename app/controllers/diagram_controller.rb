0b0a55a7552421ff4b500a5d5b451d395858ac44
colma <colma@ecoworkinc.com>
2015-11-16 16:57:36 +0800

    [#18205] 統計報表- 5_3,5_4,5_5 * add bar type in view * add modle type in controller * Add 3 methods in graph_data
    ref #18205 @24 hours

--- app/controllers/diagram_controller.rb
+++ app/controllers/diagram_controller.rb
@@ -10,7 +10,7 @@ class DiagramController < ApplicationController
     graph_data_number = params[:graph_data_number]
     start_date        = Date.parse("2014-11-01") # Date.parse(params[:start])
     end_date          = Date.today
-
+    @grapy_type = 'area'
     # --------------------
     # SQL data: related to data_quantity and period_scale
     # --------------------
@@ -30,108 +30,135 @@ class DiagramController < ApplicationController
     case graph_data_number
     when "1_1"
       graph_data = graph_1_1(period, start_date, end_date)
+      axis_type ='date'
     when "1_2"
       graph_data = graph_1_2(period, start_date, end_date)
+      axis_type ='date'
+    when "5_3"
+      graph_data = graph_5_3(period, start_date, end_date)
+      axis_type = 'modle'
+    when "5_4"
+      graph_data = graph_5_4(period, start_date, end_date)
+      axis_type = 'modle'
+    when "5_5"
+      graph_data = graph_5_5(period, start_date, end_date)
+      axis_type = 'modle'
+    
     # when "1_3"
     #   graph_data = graph_1_3(period, start_date, end_date)
     end
- 
-    @data_quantity = graph_data.length - 2
-    @columns_name  = graph_data[0]
-    @columns       = graph_data[1]
-    (3..@data_quantity).each do |l|
-      instance_variable_set("@data#{l-2}", graph_data[l-1])
-    end
 
-    # --------------------
-    # Logic for ploting
-    # --------------------
-    # Calculate date difference
-    case period_scale
-    when 1
-      # For date
-      date_diff  = (end_date - start_date).to_i
-    when 2
-      # For week
-      date_diff = TimeDifference.between(start_date, end_date).in_weeks.ceil.to_i
-    when 3
-      # For month
-      date_diff = TimeDifference.between(start_date, end_date).in_months.ceil.to_i
-    else
-      # For date
-      date_diff  = (end_date - start_date).to_i
-    end
+    if  axis_type == 'date'
+      @data_quantity = graph_data.length - 2
+      @columns_name  = graph_data[0]
+      @columns       = graph_data[1]
+      (3..@data_quantity).each do |l|
+        instance_variable_set("@data#{l-2}", graph_data[l-1])
+      end
 
-    # Accumulation
-    accumulation = []
-    (1..@data_quantity).each do |a|
-      accumulation[a-1] = 0
-    end
-
-    # Fill data in array per date range
-    (0..date_diff).each do |i|
-
+      # --------------------
+      # Logic for ploting
+      # --------------------
+      # Calculate date difference
       case period_scale
       when 1
-        # Fill date
-        date_string = (start_date + i).to_s
+        # For date
+        date_diff  = (end_date - start_date).to_i
       when 2
-        # Fill week
-        if i > 0
-          start_date = start_date.next_week
-        end
-        date_string = start_date.strftime("%Y-W%W")
+        # For week
+        date_diff = TimeDifference.between(start_date, end_date).in_weeks.ceil.to_i
       when 3
-        # Fill month
-        if i > 0
-          start_date = start_date.next_month
-        end
-        date_string = start_date.strftime("%Y-%b")
+        # For month
+        date_diff = TimeDifference.between(start_date, end_date).in_months.ceil.to_i
       else
-        # Fill date
-        date_string = (start_date + i).to_s
+        # For date
+        date_diff  = (end_date - start_date).to_i
       end
-      @columns[0] << date_string
 
-      # Fill single value: 
-      # @value_array = ["value of data1", "value of data2", "value of data3"]
-      # @columns = ["value of date", "value of data1", "value of data2", "value of data3"]
-      value_array = []
+      # Accumulation
+      accumulation = []
+      (1..@data_quantity).each do |a|
+        accumulation[a-1] = 0
+      end
 
-      (1..@data_quantity).each do |j|
-        value_array[j-1] = ""
+      # Fill data in array per date range
+      (0..date_diff).each do |i|
 
-        instance_variable_get("@data#{j}").any? do |k|
-          case period_scale
-          when 1
-            search_string = date_string
-            time          = k.time_axis.strftime("%Y-%m-%d")
-          when 2
-            search_string = start_date.strftime("%Y-%U")
-            time          = k.create_date.strftime("%Y-%U")
-          when 3
-            search_string = start_date.strftime("%Y-%b")
-            time          = k.create_date.strftime("%Y-%b")
-          else
-            search_string = date_string
-            time          = k.time_axis.strftime("%Y-%m-%d")
+        case period_scale
+        when 1
+          # Fill date
+          date_string = (start_date + i).to_s
+        when 2
+          # Fill week
+          if i > 0
+            start_date = start_date.next_week
           end
-
-          if time == search_string
-            value_array[j-1] = k.value_count
+          date_string = start_date.strftime("%Y-W%W")
+        when 3
+          # Fill month
+          if i > 0
+            start_date = start_date.next_month
           end
-        end
-
-        # Accumulation
-        unless value_array[j-1].blank?
-          accumulation[j-1] += value_array[j-1]
-          @columns[j] << accumulation[j-1]
+          date_string = start_date.strftime("%Y-%b")
         else
-          @columns[j] << accumulation[j-1]
+          # Fill date
+          date_string = (start_date + i).to_s
+        end
+        @columns[0] << date_string
+
+        # Fill single value: 
+        # @value_array = ["value of data1", "value of data2", "value of data3"]
+        # @columns = ["value of date", "value of data1", "value of data2", "value of data3"]
+        value_array = []
+
+        (1..@data_quantity).each do |j|
+          value_array[j-1] = ""
+
+          instance_variable_get("@data#{j}").any? do |k|
+            case period_scale
+            when 1
+              search_string = date_string
+              time          = k.time_axis.strftime("%Y-%m-%d")
+            when 2
+              search_string = start_date.strftime("%Y-%U")
+              time          = k.create_date.strftime("%Y-%U")
+            when 3
+              search_string = start_date.strftime("%Y-%b")
+              time          = k.create_date.strftime("%Y-%b")
+            else
+              search_string = date_string
+              time          = k.time_axis.strftime("%Y-%m-%d")
+            end
+
+            if time == search_string
+              value_array[j-1] = k.value_count
+            end
+          end
+
+          # Accumulation
+          unless value_array[j-1].blank?
+            accumulation[j-1] += value_array[j-1]
+            @columns[j] << accumulation[j-1]
+          else
+            @columns[j] << accumulation[j-1]
+          end
+
         end
 
       end
+    end
 
+    if axis_type == 'modle'
+      puts 'in controller'
+      @grapy_type = 'bar'
+      @data_quantity = graph_data.length - 2
+      @columns_name  = graph_data[0]
+      @columns       = graph_data[1]
+      @data = graph_data[2]
+      @data.each do | modle , values |
+        @columns[0] << modle
+        @columns[1] << values['value_count']
+      end
     end
 
   end
--- app/controllers/graph_data.rb
+++ app/controllers/graph_data.rb
@@ -25,5 +25,60 @@ module GraphData
     # @data3        = 
     # return [@columns_name, @columns, @data1, @data2, @data3]
   end
+  def graph_5_3(period="month(created_at)", start_date="2015-9-01", end_date="2015-11-01")
+    @columns_name = "沒有NAS的被邀請者人數"
+    @columns      = [["型號"],["沒有NAS的被邀請者人數"]]
+    @data1 = Hash.new
+    #row[0] = name , row[1] = count
+    sql = 'SELECT  `products`.name as `name` ,count( `accepted_users`.user_id ) 
+FROM `accepted_users` 
+  INNER JOIN `invitations` ON `accepted_users`.invitation_id =  `invitations`.id
+  INNER JOIN `devices` ON  `invitations`.device_id = `devices`.id 
+  INNER JOIN `products` ON `devices`.product_id  = `products`.id
+WHERE `user_id` NOT IN ( SELECT `user_id` from `pairings` where 1 ) 
+GROUP BY name'
+    records_array = ActiveRecord::Base.connection.execute(sql)
+    records_array.each do |row|
+      @data1[row[0]] = { "create_date" => row[0] , 'time_axis' => row[0] , 'value_count' => row[1]}
+    end
+    return [@columns_name, @columns, @data1]
+  end
 
+  def graph_5_4(period="month(created_at)", start_date="2015-9-01", end_date="2015-11-01")
+    @columns_name = "NAS 分享次數"
+    @columns      = [["型號"],["分享次數"]]
+    @data1 = Hash.new
+    #row[0] = name , row[1] = count
+    sql = 'SELECT `products`.name as \'name\' ,  count( `invitations`.id ) as \'sum\' 
+    FROM `invitations` 
+      JOIN `devices` on `invitations`.device_id = `devices`.id 
+      JOIN `products` on `devices`.product_id = `products`.id
+    WHERE 1 
+    GROUP BY name'
+    records_array = ActiveRecord::Base.connection.execute(sql)
+    records_array.each do |row|
+      @data1[row[0]] = { "create_date" => row[0] , 'time_axis' => row[0] , 'value_count' => row[1]}
+    end
+    return [@columns_name, @columns, @data1]
+  end
+
+
+  def graph_5_5(period="month(created_at)", start_date="2015-9-01", end_date="2015-11-01")
+    @columns_name = "NAS 分享資料夾使用人數-邀請成功"
+    @columns      = [["型號"],["分享資料夾使用人數-邀請成功"]]
+    @data1 = Hash.new
+    #row[0] = name , row[1] = count
+    sql = 'SELECT `products`.name as `name` , count( `accepted_users`.user_id ) as count
+FROM `accepted_users` 
+  INNER JOIN `invitations` ON `accepted_users`.invitation_id =  `invitations`.id
+  INNER JOIN `devices` ON  `invitations`.device_id = `devices`.id 
+  INNER JOIN `products` ON `devices`.product_id  = `products`.id
+WHERE `accepted_users`.status = 1
+GROUP BY name'
+    records_array = ActiveRecord::Base.connection.execute(sql)
+    records_array.each do |row|
+      @data1[row[0]] = { "create_date" => row[0] , 'time_axis' => row[0] , 'value_count' => row[1]}
+    end
+    return [@columns_name, @columns, @data1]
+  end
 end
