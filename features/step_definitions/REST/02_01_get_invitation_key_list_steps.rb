d72723fe7504d48bdd7257d9b585e489497f5567
Calvin Chu <calvinchu@ecoworkinc.com>
2016-04-06 15:52:26 +0800

    [#19577] [ECO WEB] [OB-002] Single Sign On (Server) - 整合 doorkeeper 的 access_token 及現有 PCloud 核發的 authentication_token
    * 修改get resource/1/invitation cucumber 測試
    - 傳入 authentication token
    - 傳入 access token
    - 傳入無效 token
    
    ref #19577

--- features/step_definitions/REST/02_01_get_invitation_key_list_steps.rb
+++ features/step_definitions/REST/02_01_get_invitation_key_list_steps.rb
@@ -6,9 +6,30 @@ When(/^client send a GET request to \/resource\/1\/invitation with:$/) do |table
   data = table.rows_hash
   path = '//' + Settings.environments.api_domain + "/resource/1/invitation"
 
-  authentication_token = data["authentication_token"].include?("EXPIRED") ? "" : @user.create_authentication_token
+  # authentication_token = data["authentication_token"].include?("EXPIRED") ? "" : @user.create_authentication_token
   cloud_id = data["cloud_id"].include?("INVALID") ? "" : @user.encoded_id
+
+  authentication_token = check_authentication_token(data["authentication_token"])
+
+  # if data["authentication_token"] == "VALID AUTHENTICATION TOKEN"
+  #   authentication_token = @user.create_authentication_token
+  # elsif data["authentication_token"] == "VALID ACCESS TOKEN"
+  #   access_token = Doorkeeper::AccessToken.create(:application_id => 1, :resource_owner_id => @user.id, :expires_in => 21600, scopes: "")
+  #   authentication_token = access_token.token
+  # elsif data["authentication_token"] == "REVOKED ACCESS TOKEN"
+  #   access_token = Doorkeeper::AccessToken.create(:application_id => 1, :resource_owner_id => @user.id, :expires_in => 21600, :revoked_at => Time.now, scopes: "")
+  #   authentication_token = access_token.token
+  # elsif data["authentication_token"] == "EXPIRED ACCESS TOKEN"
+  #   access_token = Doorkeeper::AccessToken.create(:application_id => 1, :resource_owner_id => @user.id, :expires_in => 21600, :created_at => Time.at(Time.now.to_i - 21700), scopes: "")
+  #   authentication_token = access_token.token
+  # elsif data["authentication_token"].include?("INVALID")
+  #   authentication_token = ""
+  # else
+  #   authentication_token = ""
+  # end
+
   last_updated_at = data["last_updated_at"].include?("EXPIRED") ? Time.now.to_i : 5.minute.ago.to_i
+
   get path, {
     cloud_id: cloud_id,
     last_updated_at: last_updated_at,
