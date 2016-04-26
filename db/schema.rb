53552a8b547822ee2e1d9bdd9a4dae969dccfd7e
ChouAndy <chouandy@ecoworkinc.com>
2016-04-26 17:13:10 +0800

    Update for current password feature
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225041417) do
+ActiveRecord::Schema.define(version: 20160318093021) do
 
   create_table "accepted_users", force: :cascade do |t|
     t.integer  "invitation_id", limit: 4, null: false
