96202fdd01ea4763ce870e0a814e41c0d8bf751f
Ryan <ryan@LiYu-KuangdeMacBook-Pro.local>
2016-04-27 12:28:25 +0800

    hide oauth flow unecessary link

---
+++ app/controllers/concerns/oauth_flow.rb
@@ -0,0 +1,16 @@
+module OauthFlow
+  extend ActiveSupport::Concern
+
+	included do
+
+		before_action :set_oauth_flow_flag
+
+		def set_oauth_flow_flag
+			if request.path.match("/oauth")
+				warden.session['in_oauth_flow'] = Time.now.to_i
+			end
+
+		end
+	end
+
+end
