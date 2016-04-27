5920691536f66bf16e4ddbcbb181351d14714eef
Ryan <ryan@LiYu-KuangdeMacBook-Pro.local>
2016-04-27 14:05:07 +0800

    fix fb/google login error

--- app/controllers/concerns/oauth_flow.rb
+++ app/controllers/concerns/oauth_flow.rb
@@ -6,11 +6,14 @@ module OauthFlow
 		before_action :set_oauth_flow_flag
 
 		def set_oauth_flow_flag
-			if request.path.match("/oauth")
-				warden.session['in_oauth_flow'] = Time.now.to_i
+			if current_user
+				if request.path.match("/oauth")
+					warden.session['in_oauth_flow'] = Time.now.to_i
+				end
 			end
-
 		end
+
+		
 	end
 
 end
