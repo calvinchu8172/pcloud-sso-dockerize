bbadd7d219860fdf365cc750a66d3bc2413f1394
Calvin Chu <calvinchu@ecoworkinc.com>
2016-07-14 16:10:16 +0800

    Remove the unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -51,60 +51,10 @@ Rails.application.routes.draw do
       end
     end
 
-    get 'ddns/:id', to: 'ddns#show'
-    get 'ddns/success/:id', to: 'ddns#success'
-    get 'ddns/failure/:id', to: 'ddns#failure'
-    post 'ddns/check'
-
-    get 'discoverer/index', to: 'discoverer#index'
-    get 'discoverer/add'
-    get 'discoverer/check/:id', to: 'discoverer#check'
-    get 'discoverer/indicate/:id', to: 'discoverer#indicate'
-    post 'discoverer/search'
-
-    get 'personal/index'
-    get 'personal/profile'
-    get 'personal/device_info/:id', to: 'personal#device_info'
-    get 'personal/check_status/:id', to: 'personal#check_status'
-
     unless Rails.env.production?
       mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     end
 
-    get 'pairing/index/:id', to: 'pairing#index'
-    get 'pairing/waiting/:id', to: 'pairing#waiting'
-    get 'pairing/check_connection/:id', to: 'pairing#check_connection'
-    get 'pairing/cancel/:id', to: 'pairing#cancel'
-
-    resources :package, only: [ :show, :edit, :update ]
-    get 'package/check/:id' , to: 'package#check'
-    get 'package/cancel/:id', to: 'package#cancel', as: 'cancel_package'
-
-    concern :upnp_mods do
-      resources :upnp, only: [ :show, :edit, :update ]
-      get 'upnp/cancel/:id', to: 'upnp#cancel', format: 'json'
-      get 'upnp/check/:id', to: 'upnp#check', format: 'json'
-    end
-
-    scope :path => '/1/', :module => 'mods/v1' do
-      concerns :upnp_mods
-    end
-
-    scope :path => '/2/', :module => 'mods/v2' do
-      concerns :upnp_mods
-      get 'upnp/reload/:id', to: 'upnp#reload', format: 'json'
-    end
-
-    get 'unpairing/index/:id', to: 'unpairing#index', as: 'unpairing_index'
-    get 'unpairing/success/:id', to: 'unpairing#success', as: 'unpairing_success'
-    get 'unpairing/destroy/:id', to: 'unpairing#destroy', as: 'unpairing_destroy'
-
-    get 'invitations/accept/:id', to: 'invitations#accept'
-    get 'invitations/accept', to: 'invitations#accept'
-    get 'invitations/check_connection/:id', to: 'invitations#check_connection'
-
-    resources :products, :path => "fs2g0a2vtz", except: [:destroy]
-    get 'diagram', to: 'diagram#index'
   end
 
   # Routes for Pcloud REST API server
