efed64551b2a8f86948b7cdc2bb3bc7460d7b815
Calvin Chu <calvinchu@ecoworkinc.com>
2017-02-15 14:18:57 +0800

    Remove unnecessary comments.

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,14 +7,6 @@ FactoryGirl.define do
     edm_accept                "0"
     agreement                 "1"
   end
-
-  # factory :oauth_user, class: User do
-  #   email                     "personal@example.com"
-  #   password                  "12345678"
-  #   password_confirmation     "12345678"
-  #   edm_accept                "0"
-  #   agreement                 "1"
-  # end
   # factory :device do
   #   sequence(:serial_number)  { |n| "1234567890#{n}"}
   #   sequence(:mac_address)    { |n| "#{n}".rjust(12, "0")}
