fce6f29014c39890e24bc01d6c1a27c01f36dfb1
Calvin Chu <calvinchu@ecoworkinc.com>
2019-10-28 20:02:34 +0800

    Recover Redis connection.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-# Redis.current = Redis.new(:host => Settings.redis.web_host, :port => Settings.redis.port)
-# XmppPresence.xmpp_redis = Redis.new(:host => Settings.redis.xmpp_host, :port => Settings.redis.port)
+Redis.current = Redis.new(:host => Settings.redis.web_host, :port => Settings.redis.port)
+XmppPresence.xmpp_redis = Redis.new(:host => Settings.redis.xmpp_host, :port => Settings.redis.port)
